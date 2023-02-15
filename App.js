07b8f09d9cb8dd13be2206eade4226ee4001edf7
Ssali Benjamin <ssalibenjamin0402@gmail.com>
2023-02-15 23:51:43 +0300

    Started the designing

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.circle}>
+
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +15,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ADD8E6',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  circle : {
+    padding: 30,
+    backgroundColor: "#191970",
+    width : Dimensions.get("window").width*1.8,
+    height: Dimensions.get("window").width*1.8,
+    borderRadius: Dimensions.get("window").width,
+    position: "absolute",
+    bottom:-Dimensions.get("window").height + Dimensions.get("window").width*1.5
+  }
 });

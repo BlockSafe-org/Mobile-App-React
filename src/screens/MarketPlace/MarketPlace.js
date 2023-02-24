cfdf7eae81a21cad2b20aa92f7053b97964927b1
Ssali Benjamin <ssalibenjamin0402@gmail.com>
2023-02-24 23:49:04 +0300

    Started working on Blockly

--- src/screens/MarketPlace/MarketPlace.js
+++ src/screens/MarketPlace/MarketPlace.js
@@ -1,11 +1,11 @@
-import {Button, Text, View, StyleSheet, TextInput, TouchableNativeFeedback } from 'react-native';
+import {Button, Text, View, StyleSheet, Image, Dimensions } from 'react-native';
 import globalStyles from "../../styles/GlobalStyles"
 import { useNavigation } from '@react-navigation/native';
 
 export default function MarketPlace() {
     return(
-        <View style={[styles.container, globalStyles.mainColor]}>
-            <Text>Marketplace</Text>
+        <View style={[styles.container, globalStyles.dashboardColor]}>
+            <Image style={{resizeMode:"contain",width:Dimensions.get("screen").width-50, alignSelf:"center"}} source={require("../../assets/blockly.png")}/>
         </View>
     )
 }
@@ -14,6 +14,5 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignContent: "center",
-        justifyContent: "center"
     }
 })

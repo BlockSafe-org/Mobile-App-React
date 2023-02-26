246dcadce594909914521beaf49a3a0fe735e06e
Ssali Benjamin <ssalibenjamin0402@gmail.com>
2023-02-26 09:51:46 +0300

    Added screens for Marketplace

--- src/screens/MarketPlace/Merchant.js
+++ src/screens/MarketPlace/Merchant.js
@@ -1,10 +1,11 @@
 import { Entypo } from "@expo/vector-icons";
 import { AppBar, IconButton } from "@react-native-material/core";
-import { Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import globalStyles from "../../styles/GlobalStyles";
 
 
-export default function Merchant() {
+export default function Merchant({navigation}) {
     return (
         <SafeAreaView style={[styles.container, globalStyles.dashboardColor]}>
         <AppBar
@@ -12,8 +13,15 @@ export default function Merchant() {
         titleStyle={{color:"#5F9EA0", fontSize:25}}
         leading={<IconButton icon={<Entypo name="menu" size={26} color="black" />} onPress={() => navigation.openDrawer()}/>}
         />
-        <FlatList/>
         <Text>Merchant</Text>
+        <FlatList/>
     </SafeAreaView>
         )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignContent: "center",
+    }
+})
